5109a8f6ad0add8c7a309aecc4f6e7f533081a4c
FRA2721 <cimino.francesco36@gmail.com>
2022-12-13 16:12:23 +0100

    construct added

--- Models/Movie.php
+++ Models/Movie.php
@@ -2,16 +2,30 @@
 <?php
 
 // define Movie class 
+
+/**
+ * class Movie
+ * define Movie class
+ * @author FRANCESCO CIMINO
+ */
+
 class Movie{
 
     // common main characteristics
-    public $title;
-    public $movie_director;
-    public $production;
-    public $duration;
-    public $release_date;
-    public $genre;
-    public $description;
+    // attributes
+
+    // construct
+    public function __construct(public String $title, public String $movie_director, public String $production, public int $duration, public int $release_date, public String $genre, public String $description) {
+
+        $this->title = $title;
+        $this->movie_director = $movie_director;
+        $this->production = $production;
+        $this->duration = $duration;
+        $this->release_date = $release_date;
+        $this->genre = $genre;
+        $this->description = $description;
+        
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!-- php -->
 <?php
 
+// "import" Movie class
 require_once __DIR__ .  "/Models/Movie.php";
 
 ?>
