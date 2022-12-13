42f1866af2f7ca9db4e87727ae967b5c2b44c6b4
FRA2721 <cimino.francesco36@gmail.com>
2022-12-13 17:16:59 +0100

    second example output

--- index.php
+++ index.php
@@ -6,7 +6,10 @@ require_once __DIR__ .  "/Models/Movie.php";
 
 $matrix = new Movie("Matrix", "Andy Wachowski and Larry Wachowsky", "Aleksandar Hemon, David Mitchell e Lana Wachowski", 120, 1999, "Action", "Example of movie description");
 
+$nope = new Movie("Nope", "Jordan Peele", "Jordan Peele", 120, 2022, "Fantasy", "Example of movie desciption 2");
+
 echo $matrix->getData();
+echo $nope->getData();
 
 ?>
 <!-- /php -->
