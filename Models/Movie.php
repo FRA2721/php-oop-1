29ddcdf875894b18c6ca34645b6b6843e3d94fbc
FRA2721 <cimino.francesco36@gmail.com>
2022-12-13 15:50:25 +0100

    Movie class defined

---
+++ Models/Movie.php
@@ -0,0 +1,18 @@
+<!-- php -->
+<?php
+
+// define Movie class 
+class Movie{
+
+    // common main characteristics
+    public $title;
+    public $movie_director;
+    public $production;
+    public $duration;
+    public $release_date;
+    public $genre;
+    public $description;
+}
+
+?>
+<!-- /php -->
